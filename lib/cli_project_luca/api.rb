5b91a24bd3b7eeb564730f917986efe284f7d0a9
csipsz <lucakanyo@protonmail.com>
2020-06-15 18:48:55 -0400

    choosing and testing API

--- lib/cli_project_luca/api.rb
+++ lib/cli_project_luca/api.rb
@@ -1,2 +1,10 @@
+
 class API 
+    BASE_URL = "https://the-one-api.herokuapp.com/v1/"
+    KEY = "5DDdK9dPmf_GvAAdcHcx"
+
+    def self.get_characters 
+        response = RestClient.get(BASE_URL + "book")
+        puts response
+    end 
 end 
--- lib/environment.rb
+++ lib/environment.rb
@@ -1,5 +1,13 @@
+#adding gems here so all of my files listed under have access to them
+require 'pry'
+require 'json'
+require 'rest-client'
+
 require_relative './cli_project_luca/version'
 require_relative './cli_project_luca/cli'
+require_relative './cli_project_luca/api'
+require_relative './cli_project_luca/character'
+require_relative './cli_project_luca/things'
 
 module CliProjectLuca
   class Error < StandardError; end
