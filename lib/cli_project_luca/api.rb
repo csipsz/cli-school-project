98583651f5d8b8f8407379b6f7d83924d490f167
csipsz <lucakanyo@protonmail.com>
2020-06-15 21:42:50 -0400

    creating base logic for the user interface

--- lib/cli_project_luca/api.rb
+++ lib/cli_project_luca/api.rb
@@ -3,8 +3,15 @@ class API
     BASE_URL = "https://the-one-api.herokuapp.com/v1/"
     KEY = "5DDdK9dPmf_GvAAdcHcx"
 
-    def self.get_characters 
-        response = RestClient.get(BASE_URL + "book")
-        puts response
-    end 
+    GHIBLI = "https://ghibliapi.herokuapp.com/"
+
+    #def self.get_characters 
+        #response = RestClient.get(BASE_URL + "book")
+        #puts response
+    #end 
+
+    #def self.get_characters 
+        #response = RestClient.get(GHIBLI + "people")
+        #puts response
+    #end 
 end 
--- lib/cli_project_luca/cli.rb
+++ lib/cli_project_luca/cli.rb
@@ -1,5 +1,55 @@
+require 'pry'
+
 class CLI 
     def call 
-        puts "Testing the program"
+        greet_user
+        print_menu
     end 
+
+    def greet_user 
+        puts <<-'EOF'
+         __      __       .__                                                              ._.
+        /  \    /  \ ____ |  |   ____  ____   _____   ____        __ __  ______ ___________| |
+        \   \/\/   // __ \|  | _/ ___\/  _ \ /     \_/ __ \      |  |  \/  ___// __ \_  __ \ |
+         \        /\  ___/|  |_\  \__(  <_> )  Y Y  \  ___/      |  |  /\___ \\  ___/|  | \/\|
+          \__/\  /  \___  >____/\___  >____/|__|_|  /\___  > /\  |____//____  >\___  >__|   __
+               \/       \/          \/            \/     \/  )/             \/     \/       \/
+EOF
+    end 
+
+    def print_menu 
+        user_choice = nil 
+        while user_choice != 'exit'
+            puts "Please type an option that interest you, or type 'exit' to exit!"
+            puts "Type 1 to list characters or type 2 to list movies!"
+            user_choice = gets.strip.downcase
+            if user_choice.to_i == 1
+                show_characters
+            elsif user_choice.to_i == 2 
+                show_movies
+            elsif user_choice == 'exit'
+                finishing_program
+            else 
+                puts "Invalid input, please try again or type 'exit' to exit the program!"
+            end 
+        end 
+    end 
+
+    def show_characters
+        puts "Here are your characters"
+    end 
+
+    def show_movies 
+        puts "Here are your movies"
+    end 
+
+    def finishing_program 
+       puts "Thank you for checking out my gem, I hope you had some fun!"
+    end 
+
+
+
 end 
+
+
+
