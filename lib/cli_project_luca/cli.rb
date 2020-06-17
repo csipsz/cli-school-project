7dcb5688cd7601c3eaf85f81b92079fe4df8bd21
csipsz <lucakanyo@protonmail.com>
2020-06-16 22:15:45 -0400

    refinign user interface

--- lib/cli_project_luca/cli.rb
+++ lib/cli_project_luca/cli.rb
@@ -3,6 +3,9 @@ require 'pry'
 class CLI 
     def call 
         greet_user
+        API.get_characters
+        API.get_places 
+        API.get_films
         print_menu
     end 
 
@@ -21,9 +24,9 @@ EOF
     end 
 
     def print_menu 
-        user_choice = nil 
         puts "This is a small database about the Ghibli universe.".colorize(:cyan)
         puts "Please type an option that interest you, or type 'exit' to exit!".colorize(:cyan)
+        user_choice = nil
         while user_choice != 'exit'
             puts "Type 1 to list characters or type 2 to list imaginary places, or type exit!".colorize(:magenta)
             user_choice = gets.strip.downcase
@@ -32,16 +35,17 @@ EOF
             elsif user_choice.to_i == 2 
                 show_places
             elsif user_choice == 'exit'
-                finishing_program
+                finishing_program 
             else 
                 puts "Invalid input, please try again or type 'exit' to exit the program!".colorize(:red)
             end 
         end 
+        #finishing_program
     end 
 
     def show_characters
         puts "Here are all of your characters".colorize(:light_magenta)
-        API.get_characters
+        #API.get_characters
         Character.all.each_with_index do |character, index| 
             puts "#{index + 1} #{character.name}" 
         end 
@@ -50,8 +54,7 @@ EOF
 
     def show_places 
         puts "Here are all of the locations!".colorize(:light_magenta)
-        API.get_characters
-        API.get_places
+        #API.get_places
         Place.all.each_with_index do |place, index| 
         puts "#{index + 1} #{place.name}" 
         end 
@@ -68,12 +71,12 @@ EOF
         Character.all.each do |character| 
             if character.name == character_choice
                 puts "#{character.name} is a great choice!" 
-                puts "He'/She is #{character.age} a years old #{character.gender}."
-                puts "He'/She has #{character.eye_color} eyes and wonderful #{character.hair_color} hair!"
-                puts "He/She appeared in: #{character.character_movie}"
+                puts "#{character.name} is a #{character.age} years old #{character.gender}."
+                puts "#{character.name} has #{character.eye_color} eyes and wonderful #{character.hair_color} hair!"
+                puts "Known from: #{character.character_movie}"
             end 
         end 
-       continue_on
+       movie_info
     end 
 
     def select_places 
@@ -83,20 +86,28 @@ EOF
         Place.all.each do |place| 
             if place.name == place_choice
                 puts "#{place.name} has a #{place.climate} climate with the terrain of a #{place.terrain}." 
+                puts "Unfortunately there is no more data about this place!"
             end 
         end 
     end 
 
 
-    def continue_on
-        puts "If you are interested in that movie, type in the title!"
+    def movie_info
+        puts "If you are interested in that movie, type in the title!".colorize(:yellow)
         current_movie = gets.chomp.split(" ").each{|word| word.downcase!}.join(" ")
-        API.get_films
+        #API.get_films
         Film.all.each do |film|
             if film.title.split(" ").each{|word| word.downcase!}.join(" ") == current_movie
-                puts "#{film.title}".colorize(:ligt_cyan)
+                puts film.title.upcase.colorize(:light_cyan)
                 puts film.description
             end  
+        end 
+        puts "Type 'main' if you want to get back to main menu" 
+        user_input = gets.strip 
+        if user_input == 'main'
+            print_menu 
+        else 
+            puts "I am not sure what you would like to do. Can you please try again?"
         end 
     end 
 
