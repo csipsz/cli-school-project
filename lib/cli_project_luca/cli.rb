539da0f4b6606fd03522c89115de417caac034b6
csipsz <lucakanyo@protonmail.com>
2020-06-17 12:09:19 -0400

    adding more error messages for typos

--- lib/cli_project_luca/cli.rb
+++ lib/cli_project_luca/cli.rb
@@ -40,12 +40,10 @@ EOF
                 puts "Invalid input, please try again or type 'exit' to exit the program!".colorize(:red)
             end 
         end 
-        #finishing_program
     end 
 
     def show_characters
         puts "Here are all of your characters".colorize(:light_magenta)
-        #API.get_characters
         Character.all.each_with_index do |character, index| 
             puts "#{index + 1} #{character.name}" 
         end 
@@ -54,7 +52,6 @@ EOF
 
     def show_places 
         puts "Here are all of the locations!".colorize(:light_magenta)
-        #API.get_places
         Place.all.each_with_index do |place, index| 
         puts "#{index + 1} #{place.name}" 
         end 
@@ -63,32 +60,43 @@ EOF
 
     def finishing_program 
        puts "Thank you for checking out my gem, I hope you had some fun!".colorize(:cyan)
+       exit 
     end 
 
     def select_character 
         puts 'Enter the name of the character that you would like to learn more about!'.colorize(:yellow)
         character_choice = gets.chomp.split(" ").each{|word| word.capitalize!}.join(" ")
-        Character.all.each do |character| 
-            if character.name == character_choice
-                puts "#{character.name} is a great choice!" 
-                puts "#{character.name} is a #{character.age} years old #{character.gender}."
-                puts "#{character.name} has #{character.eye_color} eyes and wonderful #{character.hair_color} hair!"
-                puts "Known from: #{character.character_movie}"
+        if Character.all.any?{|item| item.name == character_choice}
+            Character.all.each do |character| 
+                if character.name == character_choice
+                    puts "#{character.name} is a great choice!" 
+                    puts "#{character.name} is a #{character.age} years old #{character.gender}."
+                    puts "#{character.name} has #{character.eye_color} eyes and wonderful #{character.hair_color} hair!"
+                    puts "Known from: #{character.character_movie}"
+                end 
             end 
-        end 
-       movie_info
+        movie_info
+        else 
+        puts "I think you made a typo! Can you try it again please?".colorize(:red)
+        select_character
+    end 
     end 
 
     def select_places 
         puts "Which place would you like to discover?".colorize(:yellow)
         puts "Type in the name that you are interested in!".colorize(:yellow)
         place_choice = gets.chomp.split(" ").each{|word| word.capitalize!}.join(" ")
-        Place.all.each do |place| 
-            if place.name == place_choice
-                puts "#{place.name} has a #{place.climate} climate with the terrain of a #{place.terrain}."
-                puts "It is shown in #{place.get_movie_from_url}. " 
-                puts "Unfortunately there is no more data about this place!"
+        if Place.all.any?{|item| item.name == place_choice}
+            Place.all.each do |place| 
+                if place.name == place_choice
+                    puts "#{place.name} has a #{place.climate} climate with the terrain of a #{place.terrain}."
+                    puts "It is shown in #{place.get_movie_from_url}. " 
+                    puts "Unfortunately there is no more data about this place!"
+                end 
             end 
+        else
+            puts "I think that place only lives in your imagination, but unfortunately not in my database! Can you try it again please?".colorize(:red)
+            select_places 
         end 
     end 
 
@@ -96,7 +104,6 @@ EOF
     def movie_info
         puts "If you are interested in that movie, type in the title!".colorize(:yellow)
         current_movie = gets.chomp.split(" ").each{|word| word.downcase!}.join(" ")
-        #API.get_films
         Film.all.each do |film|
             if film.title.split(" ").each{|word| word.downcase!}.join(" ") == current_movie
                 puts film.title.upcase.colorize(:light_cyan)
@@ -104,13 +111,7 @@ EOF
             end  
         end 
         puts ' '
-        puts "Type 'main' if you want to get back to main menu".colorize(:yellow) 
-        user_input = gets.strip 
-        if user_input == 'main'
-            print_menu
-        else 
-            puts "I am not sure what you would like to do. Can you please try again?"
-        end 
+        puts "Feel free to look around more!".colorize(:yellow) 
     end 
 
 end 
